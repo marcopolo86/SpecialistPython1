5cbe53903657340d236cd82c7f818212755ecd18
marcopolo86 <107918308+marcopolo86@users.noreply.github.com>
2022-06-24 12:28:36 +0300

    Update [hw]01_task_string.py

--- Module3/home_work/string/[hw]01_task_string.py
+++ Module3/home_work/string/[hw]01_task_string.py
@@ -1,4 +1,17 @@
 # Дан произвольный текст. Каких символов больше, точек "." или запятых ","?
 # Если точек и запятых одинаково - выведите "одинаково"
 
-text = ...
+new_string = input()
+
+# new_string = ',,,!!!///....'
+
+comma = new_string.count(',')
+point = new_string.count('.')
+print(comma)
+print(point)
+if comma > point:
+    print("больше запятых")
+elif comma < point:
+    print("больше точек")
+elif comma == point:
+    print("одинаково")
