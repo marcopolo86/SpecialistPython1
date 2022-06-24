55372491ab9086a2a421f97f1060e471aaa46b5a
marcopolo86 <107918308+marcopolo86@users.noreply.github.com>
2022-06-24 16:09:46 +0300

    Update [hw]03_task_string.py

--- Module3/home_work/string/[hw]03_task_string.py
+++ Module3/home_work/string/[hw]03_task_string.py
@@ -1,3 +1,28 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+# б, в, г, д, з, к, л, м, н, п, р, с, т, ф, х.
+
+text = input('текст: ')
+sum_1 = text.count('б')
+sum_2 = text.count('в')
+sum_3 = text.count('г')
+sum_4 = text.count('д')
+sum_5 = text.count('ж')
+sum_6 = text.count('з')
+sum_7 = text.count('й')
+sum_8 = text.count('к')
+sum_9 = text.count('л')
+sum_10 = text.count('м')
+sum_11 = text.count('н')
+sum_12 = text.count('п')
+sum_13 = text.count('р')
+sum_14 = text.count('с')
+sum_15 = text.count('т')
+sum_16 = text.count('ф')
+sum_17 = text.count('х')
+sum_18 = text.count('ц')
+sum_19 = text.count('ч')
+sum_20 = text.count('ш')
+sum_21 = text.count('щ')
+print(sum_1 + sum_2 + sum_3 + sum_4 + sum_5 + sum_6 + sum_7 + sum_8 + sum_9 + sum_10 + sum_11 + sum_12 + sum_13 + sum_14 + sum_15 + sum_16 + sum_17 + sum_18 + sum_19 + sum_20 + sum_21)
+
